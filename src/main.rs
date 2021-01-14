8a92f2129fcc93f20e02695a245b1f4b4ed9cd07
Radim Janoš <radim.janos@apitree.cz>
2021-01-14 21:53:13 +0100

    view of grid and stone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use yew::prelude::*;
+
+#[derive(Clone)]
+struct Cell {
+    filled: bool,
+}
+impl Cell {
+    pub fn new() -> Self {
+        Self { filled: false }
+    }
+}
+
+pub struct Board {
+    cells: Vec<Cell>,
+}
+impl Component for Board {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        let size: usize = 19;
+        Self {
+            cells: vec![Cell::new(); size.pow(2)],
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        unimplemented!()
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        let cells = self.cells.iter().map(|_| html! {<div class="grid-item"/>});
+
+        html! {
+            <div class="grid-container">
+                {for cells}
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Board>();
 }
