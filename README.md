1f8460fea91e293b08d8316a999eead73fe96edd
Radim Janoš <radim.janos@apitree.cz>
2021-01-15 00:53:08 +0100

    filling cells (placing stones)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Yewban
+A simple frontend in Rust for a goban.
+
+## Static files
+The folder /static and its contents are copied via trunk automatically to /dist thanks to: \
+index.html: `<link data-trunk rel="copy-dir" href="path/to/images-dir"/>`
--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,47 @@ struct Cell {
     filled: bool,
 }
 impl Cell {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self { filled: false }
+    }
+    fn fill(&mut self) {
+        self.filled = true;
     }
 }
 
+pub enum Msg {
+    FillCell(usize),
+}
+
 pub struct Board {
+    link: ComponentLink<Self>,
     cells: Vec<Cell>,
 }
 impl Component for Board {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
-    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        // changing this size requires to also change the grid-template-columns in the CSS
         let size: usize = 19;
         Self {
+            link,
             cells: vec![Cell::new(); size.pow(2)],
         }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        unimplemented!()
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::FillCell(id) => {
+                let cell = self.cells.get_mut(id).unwrap();
+                if cell.filled {
+                    false
+                } else {
+                    cell.fill();
+                    true
+                }
+            }
+        }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -33,7 +53,14 @@ impl Component for Board {
     }
 
     fn view(&self) -> Html {
-        let cells = self.cells.iter().map(|_| html! {<div class="grid-item"/>});
+        let cells = self.cells.iter().enumerate().map(|(id, cell)| {
+            let class = if cell.filled {
+                "grid-item-with-stone"
+            } else {
+                "grid-item"
+            };
+            html! {<div class=class onclick=self.link.callback(move |_| Msg::FillCell(id)) />}
+        });
 
         html! {
             <div class="grid-container">
